763a5363afc5106cf62ec5c75b899a93973a7eaf
Ylva Svendsen <ylva.magnusson.svendsen@gmail.com>
2025-09-13 09:54:36 +0200

    Add files via upload

---
+++ OppgaveG4.java
@@ -0,0 +1,26 @@
+public class OppgaveG4 {
+
+  public static void main(String[] args) {
+
+    int i;
+
+    System.out.println("Test av repetisjonssetning.");
+    System.out.println("***************************");
+
+    i = 1;
+
+    while (i <= 10) {
+      System.out.println("Dette er linje nr. " + i);
+
+      if (i == 4) {
+        System.out.println();
+      }
+
+      if (i == 8) {
+        System.out.println();
+      }
+
+      i = i + 1;
+    }
+  }
+}
